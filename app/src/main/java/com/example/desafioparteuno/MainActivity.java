9acccb9d8adb4cfc241c52a925f9143c1d62e69c
stbanv <stbanv@gmail.com>
2020-07-08 22:37:22 -0400

    'DesafioAndroid'

---
+++ app/src/main/java/com/example/desafioparteuno/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.desafioparteuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivLogo;
+    private EditText etUserName;
+    private EditText etPass;
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivLogo = findViewById(R.id.ivLogo);
+        etUserName = findViewById(R.id.etUserName);
+        etPass = findViewById(R.id.etPass);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+
+        Picasso.get().load("https://desafiolatam.com/assets/home/logo-academia-bla-790873cdf66b0e681dfbe640ace8a602f5330bec301c409744c358330e823ae3.png").into(ivLogo);
+
+
+
+    }
+}
